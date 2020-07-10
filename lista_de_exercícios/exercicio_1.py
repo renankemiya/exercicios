f1236dbf49e6015598bf9ed0ed44e5781f42e12b
renankemiya <renankemiya@gmail.com>
2020-07-10 12:35:06 -0300

    editando

--- lista_de_exercícios/exercicio_1.py
+++ lista_de_exercícios/exercicio_1.py
@@ -1,11 +1,11 @@
 def oi(s):
-    '''Função que imprime a mensagem 'Olá Mundo' com um str especifico
+    '''Função que imprime a mensagem 'Alo Mundo' com um str especifico
 
     :param s:
     :return:
     '''
     if s == 'oi':
-        print('Olá Mundo')
+        print('Alo Mundo')
     elif s != 'oi':
         print('tente novamente')
 
