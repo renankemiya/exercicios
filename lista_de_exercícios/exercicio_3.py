a5467ca24a823a5acbe9088ebc51fd73616ca983
Windows10 <renankemiya@gmail.com>
2020-07-15 17:23:34 -0300

    tentando melhorar resposta exercicio_4.py

--- lista_de_exercícios/exercicio_3.py
+++ lista_de_exercícios/exercicio_3.py
@@ -1,9 +1,10 @@
 def somar(parametro, parametro_2):
-    return parametro+parametro_2
+    return parametro + parametro_2
+
 
 class Calcular():
 
-    def juntar(self, numero_1 = 0, numero_2 = 0):
+    def juntar(self, numero_1=0, numero_2=0):
         self.numero_1 = numero_1
         self.numero_2 = numero_2
         numero_s = numero_1 + numero_2
@@ -17,5 +18,4 @@ if __name__ == '__main__':
     resultado.numero_2 = 300
     print(resultado.numero_1)
     print(resultado.numero_2)
-    print(resultado.juntar(4000,20))
-    
+    print(resultado.juntar(4000, 20))
--- lista_de_exercícios/exercicio_4.py
+++ lista_de_exercícios/exercicio_4.py
@@ -1,1 +1,41 @@
-def
+def nota_media(nota1=0.0, nota2=0.0, nota3=0.0, nota4=0.0):
+    return (nota1 + nota2 + nota3 + nota4) / 4
+
+
+class NotaMedia:
+    def __init__(self, valor1=0.0, valor2=0.0, valor3=0.0, valor4=0.0):
+        self.valor1 = valor1
+        self.valor3 = valor3
+        self.valor2 = valor2
+        self.valor4 = valor4
+
+    def somar(self):
+        media = self.valor1 + self.valor2 + self.valor3 + self.valor4
+        return media / 4
+
+
+class CalcularMedia:
+
+    def __init__(self, bimestre1=0.0, bimestre2=0.0, bimestre3=0.0, bimestre4=0.0, notatotal=0.0):
+        self.bimestre1 = bimestre1
+        self.bimestre2 = bimestre2
+        self.bimestre3 = bimestre3
+        self.bimestre4 = bimestre4
+        self.notatotal = notatotal
+
+    def juntar_e_dividir(self):
+        self.notatotal = self.bimestre1 + self.bimestre2 + self.bimestre3 + self.bimestre4
+        return self.notatotal / 4
+
+
+if __name__ == '__main__':
+    print(nota_media(7.4, 5.9, 5.4, 9.3))
+    resultado = NotaMedia()
+    resultado.valor1 = 2.3
+    resultado.valor2 = 5.4
+    resultado.valor3 = 8.7
+    resultado.valor4 = 4.9
+    print(resultado.somar())
+    resultado2 = CalcularMedia()
+    resultado2.bimestre1 = 4
+    print(resultado2.juntar_e_dividir())
