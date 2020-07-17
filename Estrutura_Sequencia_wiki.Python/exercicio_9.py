e786f6cfca7ee0a5f7ab5c1042099efd5ced9856
Windows10 <renankemiya@gmail.com>
2020-07-17 16:09:30 -0300

    terminado exercicio_9.py

--- Estrutura_Sequencia_wiki.Python/exercicio_9.py
+++ Estrutura_Sequencia_wiki.Python/exercicio_9.py
@@ -5,6 +5,16 @@ class Temperatura:
     def converter_farenheit_celsius(self):
         celsius = self.farenheit - 32
         celsius = (celsius / 9) * 5
-        return celsius
+        return celsius, 'Graus Celsius'
+
+
+def converter_teperatura(farenheit):
+    celsius = farenheit - 32
+    celsius = (celsius / 9) * 5
+    return celsius, 'Graus Celsius'
+
 
 if __name__ == '__main__':
+    temperatura = Temperatura(122)
+    print(temperatura.converter_farenheit_celsius())
+    print(converter_teperatura(410))
