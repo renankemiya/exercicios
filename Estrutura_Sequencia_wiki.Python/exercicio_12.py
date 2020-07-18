ca941d2162737bf0c49886a4bdc58f35a2fbe94f
Windows10 <renankemiya@gmail.com>
2020-07-18 13:36:53 -0300

    terminando exercicio_12.py

--- Estrutura_Sequencia_wiki.Python/exercicio_12.py
+++ Estrutura_Sequencia_wiki.Python/exercicio_12.py
@@ -2,11 +2,11 @@ class PesoIdeal:
     def __init__(self, altura):
         self.altura = altura
 
-    def clacular_peso_ideal(self):
+    def calcular_peso_ideal(self):
         peso = self.altura * 72.7
         peso = peso - 58
         return f'O peso ideal é de {peso} Kg'
 
 if __name__ == '__main__':
     peso = PesoIdeal(2.10)
-    print(peso.clacular_peso_ideal())
+    print(peso.calcular_peso_ideal())
