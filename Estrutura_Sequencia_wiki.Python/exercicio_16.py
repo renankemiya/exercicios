6dc3308ae77e244ebf261da01a24c28871ba18af
Windows10 <renankemiya@gmail.com>
2020-07-18 16:02:03 -0300

    terminando exercicio_16.py

--- Estrutura_Sequencia_wiki.Python/exercicio_16.py
+++ Estrutura_Sequencia_wiki.Python/exercicio_16.py
@@ -1,8 +1,8 @@
 class CalculadorDeTinta:
     def __init__(self, area):
-        self.area = area # metros quadrados
-        self.lata_volume = 18 # litros
-        self.preco = 80.00 # reais
+        self.area = area  # metros quadrados
+        self.lata_volume = 18  # litros
+        self.preco = 80.00  # reais
 
     def cobertura_por_litro(self):
         lata = 0
@@ -13,7 +13,6 @@ class CalculadorDeTinta:
             unidade_lata -= 1
         custo = lata * self.preco
         return f'Em {self.area} metros quadrados são necessarias {lata} latas de tinta, com o preço de {custo} Reais'
-
 
 
 if __name__ == '__main__':
