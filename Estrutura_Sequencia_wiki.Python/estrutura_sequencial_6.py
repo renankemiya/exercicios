ea39380fe64d716178ef7ad37106234ef87cbdaa
Windows10 <renankemiya@gmail.com>
2020-07-20 14:07:27 -0300

    corrigindo exercicio

--- Estrutura_Sequencia_wiki.Python/estrutura_sequencial_6.py
+++ Estrutura_Sequencia_wiki.Python/estrutura_sequencial_6.py
@@ -4,21 +4,27 @@ import math
 class CalcularAreaDoCirculo:
     area_do_circulo = 0
 
-    def __init__(self, raio):
-        self.raio = raio
+    def __init__(self, raio_c):
+        self.raio_c = raio_c
 
     def calcular_area(self):
-        raio = self.raio
-        resultado = (raio ** 2) * math.pi
+        raio_f = self.raio_c
+        resultado = (raio_f ** 2) * math.pi
         return f'area do circulo = {resultado}'
 
 
-def calcular_area_circulo(raio):
-    area = (raio ** 2) * math.pi
-    return f'area do circulo = {area}'
+def calcular_area_circulo(raio_f):
+    area_f = (raio_f ** 2) * math.pi
+    return f'area do circulo = {area_f}'
 
 
 if __name__ == '__main__':
     area_do_circulo = CalcularAreaDoCirculo(5)
     print(area_do_circulo.calcular_area())
     print((calcular_area_circulo(10)))
+
+raio = int(input('Qual a medida do raio do circulo? '))  # resposta certa, corrigida da internet
+
+area = 3.1415 * raio ** 2
+
+print('A area desse circulo é: ', area)
