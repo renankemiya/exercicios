e9ac8a52074e8c21b2f04840c39ee6139ef11300
Windows10 <renankemiya@gmail.com>
2020-07-20 14:47:26 -0300

    corrigindo exercicios

--- Estrutura_Sequencia_wiki.Python/estrutura_sequencial_12.py
+++ Estrutura_Sequencia_wiki.Python/estrutura_sequencial_12.py
@@ -1,12 +1,19 @@
 class PesoIdeal:
-    def __init__(self, altura):
-        self.altura = altura
+    def __init__(self, altura_c):
+        self.altura = altura_c
 
     def calcular_peso_ideal(self):
-        peso = self.altura * 72.7
-        peso = peso - 58
-        return f'O peso ideal é de {peso} Kg'
+        peso_c = self.altura * 72.7
+        peso_c = peso_c - 58
+        return f'O peso ideal é de {peso_c} Kg'
+
 
 if __name__ == '__main__':
     peso = PesoIdeal(2.10)
     print(peso.calcular_peso_ideal())
+
+altura = float(input('Informe sua altura: '))  # resposta certa, corrigido da internet
+
+peso = 72.7 * altura - 58
+
+print('Seu peso ideal seria: ', peso, 'Kg')
