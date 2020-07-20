ab6249678bcb2271e224a6fd3eba526d7c70c860
Windows10 <renankemiya@gmail.com>
2020-07-20 13:56:19 -0300

    corrigindo exercicio

--- Estrutura_Sequencia_wiki.Python/estrutura_sequencial_5.py
+++ Estrutura_Sequencia_wiki.Python/estrutura_sequencial_5.py
@@ -2,12 +2,14 @@ class ConversorParaCentimetros:
     """ CLasse que guarda o calculo de conversão de distancia para centimetros
 
     """
-    def __init__(self, metro):
-        self.metro = metro
+
+    def __init__(self, metro_c):
+        self.metro_c = metro_c
 
     def conversor_para_centimetros(self):
-        metro = self.metro * 100
-        return f'{metro} centimetros'
+        metro_c = self.metro_c * 100
+        return f'{metro_c} centimetros'
+
 
 def metros_para_centimetros(s):
     """ Função que converte metros para centimetros
@@ -15,8 +17,9 @@ def metros_para_centimetros(s):
     :param s:
     :return:
     """
-    valor = s * 100
-    return f'{valor} centimetros'
+    valor_f = s * 100
+    return f'{valor_f} centimetros'
+
 
 if __name__ == '__main__':
     valor = ConversorParaCentimetros(1)
@@ -24,3 +27,9 @@ if __name__ == '__main__':
     valor.metro = 10
     print(valor.conversor_para_centimetros())
     print(metros_para_centimetros(1))
+
+metro = int(input('Insira a sua medida em metros: '))  # resposta certa, corrigida da internet
+
+cm = metro * 100
+
+print('Sua medida em centimetros é: ', cm, 'cm')
