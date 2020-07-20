8b9ffa8c626658bacc44c2eba5dff0526c544ced
Windows10 <renankemiya@gmail.com>
2020-07-20 15:01:16 -0300

    corrigindo exercicios

--- Estrutura_Sequencia_wiki.Python/estrutura_sequencial_13.py
+++ Estrutura_Sequencia_wiki.Python/estrutura_sequencial_13.py
@@ -17,3 +17,22 @@ if __name__ == '__main__':
     print(peso_ideal_h.calcular_peso())
     peso_ideal_m = PesoIdealPorGenero(1.80, 'mulher')
     print(peso_ideal_m.calcular_peso())
+
+altura = float(input('Qual a sua altura?: '))
+sexo = input('Você é o homem ou mulher?: ')
+peso = float(input('Qual o seu peso?: '))
+
+if sexo == 'mulher':
+    peso_ideal = (62.1*altura) - 44.7
+    print('Seu peso ideal seria:',peso_ideal)
+else:
+
+    peso_ideal = (72.7*altura) - 58
+    print('Seu peso ideal seria:',peso_ideal)
+
+if peso < peso_ideal:
+    print('Você está abaixo do seu peso ideal')
+elif peso == peso_ideal:
+    print('Você está no seu peso ideal')
+else:
+    print('Você está acima do seu peso ideal ')
