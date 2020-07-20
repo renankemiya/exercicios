f97184c190536a1c03020940eba53c95d822d2bc
Windows10 <renankemiya@gmail.com>
2020-07-20 13:07:37 -0300

    corrigindo exercicio_1.py

--- Estrutura_Sequencia_wiki.Python/exercicio_1.py
+++ Estrutura_Sequencia_wiki.Python/exercicio_1.py
@@ -1,21 +1,23 @@
 def oi(s):
-    '''Função que imprime a mensagem 'Alo Mundo' com um str especifico
+    """Função que imprime a mensagem 'Alo Mundo' com um str especifico
 
     :param s:
     :return:
-    '''
+    """
     if s == 'oi':
         print('Alo Mundo')
     elif s != 'oi':
         print('tente novamente')
 
-class Alo():
+
+def cumprimento():
+    return 'Alo Mundo'
+
+
+class Alo:
     caixa = {'oi': 'Alo Mundo'}
-    def cumprimento(self):
-        return 'Alo Mundo'
 
     def cumpirmentar_2(self):
-
         ola = self.caixa['oi']
         return f'{ola}'
 
@@ -23,5 +25,7 @@ class Alo():
 if __name__ == '__main__':
     oi('oi')
     mundo = Alo()
-    print(mundo.cumprimento())
+    print(cumprimento())
     print(mundo.cumpirmentar_2())
+
+print('Alo Mundo')  # resposta certa pego da internet
