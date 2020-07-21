ef0579cc0f9476b7ed66c397e8250940e9c02f88
Windows10 <renankemiya@gmail.com>
2020-07-21 13:46:34 -0300

    terminado estrutura_de_decisão_2.py

---
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decição_2.py
@@ -0,0 +1,8 @@
+valor = float(input('Insira um número: '))
+
+if valor > 0:
+    print('O número tem valor positivo')
+elif valor < 0:
+    print('O número tem valor negativo')
+else:
+    print('O valor é 0')
