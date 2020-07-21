c045eaddb1e66771499faa9ba165722b56abe24f
Windows10 <renankemiya@gmail.com>
2020-07-21 15:30:48 -0300

    terminado estrutura_de_decisão_5.py

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_5.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_5.py
@@ -1,2 +1,11 @@
-nota = float(input('Insira a nota: '))
+nota_1 = float(input('Insira a nota: '))
+nota_2 = float(input('Insira outro número: '))
+media = (nota_1 + nota_2) / 2
 
+
+if media == 10:
+    print('Aprovado com Distinção')
+elif media >= 7:
+    print('Aprovado')
+elif media < 7:
+    print('Reprovado')
