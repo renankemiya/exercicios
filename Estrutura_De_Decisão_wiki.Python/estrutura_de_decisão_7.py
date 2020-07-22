10ce48fdcc291e1c11a4a9c9b137c73ce0a31271
Windows10 <renankemiya@gmail.com>
2020-07-22 15:58:35 -0300

    terminado estrutura_de_decisão_6.py

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_7.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_7.py
@@ -2,17 +2,31 @@ numero_1 = float(input('Insira o primeiro número: '))
 numero_2 = float(input('Insira o segundo número: '))
 numero_3 = float(input('Insira o terceiro número: '))
 
-if numero_1 > numero_2 > numero_3:
-    print(f'O primeiro número é o maior, {numero_1}, e o terceiro é o menor, {numero_3}')
-elif numero_1 > numero_2 < numero_3:
-    print(f'O primeiro número é o maior, {numero_1}, e o segundo é o menor, {numero_2}')
-elif numero_2 > numero_3 > numero_1:
-    print(f'O segundo número é o maior, {numero_2}, e o primeiro é o menor, {numero_1}')
-elif numero_2 > numero_3 < numero_1:
-    print(f'O segundo número é o maior, {numero_2}, e o terceiro é o menor, {numero_3}')
-elif numero_3 > numero_1 > numero_2:
-    print(f'O terceiro número é o maior, {numero_3}, e o segundo é o menor, {numero_2}')
-elif numero_3 > numero_1 < numero_2:
-    print(f'O terceiro número é o maior, {numero_3}, e o segundo é o menor, {numero_1}')
+if numero_1 > numero_2:
+    if numero_1 > numero_3:
+        if numero_2 > numero_3:
+            print(f'O primeiro é o maior: {numero_1}, e o terceiro é o menor: {numero_3}')
+        elif numero_2 < numero_3:
+            print(f'O primeiro é o maior: {numero_1}, o segundo é o menor: {numero_2}')
+        else:
+            print(f'O primeiro é o ')
+    elif numero_1 < numero_3:
+        print(f'O terceiro é o maior número {numero_3}')
+    else:
+        print(f'O primeiro e o terceiro são iguais: {numero_1}, {numero_3}')
+elif numero_2 > numero_3:
+    if numero_2 > numero_1:
+        print(f'O segundo é o maior número: {numero_2}')
+    elif numero_2 < numero_1:
+        print(f'O primeiro é o maior número: {numero_1}')
+    else:
+        print(f'O segundo e o primeiro são iguais: {numero_2}, {numero_1}')
+elif numero_3 > numero_1:
+    if numero_3 > numero_2:
+        print(f'O terceiro é o maior número: {numero_3}')
+    elif numero_3 < numero_2:
+        print(f'O segundo é o maior número: {numero_2}')
+    else:
+        print(f'O terceiro e o segundo são iguais: {numero_3}, {numero_2}')
 else:
-    print('Existem números iguais')
+    print(f'Todos os números são iguais: {numero_1}, {numero_2}, {numero_3}')
