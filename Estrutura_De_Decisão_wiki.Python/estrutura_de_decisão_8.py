f3af2ac47724b8f8c8bd57ee378b904ea1237366
Windows10 <renankemiya@gmail.com>
2020-07-22 17:00:56 -0300

    terminado estrutura_de_decisão_8.py

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_8.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_8.py
@@ -2,17 +2,26 @@ preco_1 = float(input('Insira o primeiro preço: '))
 preco_2 = float(input('Insira o segundo preço: '))
 preco_3 = float(input('Insira o terceiro preço: '))
 
-if preco_1 < preco_2 < preco_3:
-    print(f'O menor preço é o do primeiro: {preco_1}')
-elif preco_1 < preco_2 > preco_3:
-    print(f'O menor preço é o do primeiro: {preco_1}')
-elif preco_2 < preco_3 < preco_1:
-    print(f'O menor preço é o do segundo: {preco_2}')
-elif preco_2 < preco_3 > preco_1:
-    print(f'O menor preço é o do segundo: {preco_2}')
-elif preco_3 < preco_1 < preco_2:
-    print(f'O menor preço é o do terceiro: {preco_3}')
-elif preco_3 < preco_1 > preco_2:
-    print(f'O menor preço é o do terceiro: {preco_3}')
+if preco_1 < preco_2:
+    if preco_1 < preco_3:
+        print(f'O primeiro preço é o menor: {preco_1}')
+    elif preco_1 > preco_3:
+        print(f'o terceiro preço é o menor: {preco_3}')
+    else:
+        print(f'O primeiro e o terceiro tem o mesmo preço: {preco_1}, {preco_3}')
+elif preco_2 < preco_3:
+    if preco_2 < preco_1:
+        print(f'O segundo preço é o menor: {preco_2}')
+    elif preco_2 > preco_1:
+        print(f'o primeiro preço é o menor: {preco_1}')
+    else:
+        print(f'O segundo e o primeiro tem o mesmo preço: {preco_2}, {preco_1}')
+elif preco_3 < preco_1:
+    if preco_3 < preco_2:
+        print(f'O terceiro preço é o menor: {preco_3}')
+    elif preco_3 > preco_2:
+        print(f'o segundo preço é o menor: {preco_2}')
+    else:
+        print(f'O terceiro e o segundo tem o mesmo preço: {preco_3}, {preco_2}')
 else:
-    print('Existem preços iguais')
+    print(f'Existem preços iguais: {preco_1}, {preco_2}, {preco_3}')
