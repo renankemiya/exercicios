9304fad1aa8991fd022ed5509fb7c8643087a0f0
Windows10 <renankemiya@gmail.com>
2020-07-22 15:51:32 -0300

    terminado estrutura_de_decisão_6.py

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_6.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_6.py
@@ -2,17 +2,26 @@ numero_1 = float(input('Insira o primeiro número: '))
 numero_2 = float(input('Insira o segundo número: '))
 numero_3 = float(input('Insira o terceiro número: '))
 
-if numero_1 > numero_2 > numero_3:
-    print(f'O primeiro número é {numero_1}')
-elif numero_1 > numero_2 < numero_3:
-    print(f'O primeiro número é {numero_1}')
-elif numero_2 > numero_3 > numero_1:
-    print(f'O segundo número é {numero_2}')
-elif numero_2 > numero_3 < numero_1:
-    print(f'O segundo número é {numero_2}')
-elif numero_3 > numero_1 > numero_2:
-    print(f'O terceiro número é {numero_3}')
-elif numero_3 > numero_1 < numero_2:
-    print(f'O terceiro número é {numero_3}')
+if numero_1 > numero_2:
+    if numero_1 > numero_3:
+        print(f'O primeiro é maior número: {numero_1}')
+    elif numero_1 < numero_3:
+        print(f'O terceiro é o maior número {numero_3}')
+    else:
+        print(f'O primeiro e o terceiro são iguais: {numero_1}, {numero_3}')
+elif numero_2 > numero_3:
+    if numero_2 > numero_1:
+        print(f'O segundo é o maior número: {numero_2}')
+    elif numero_2 < numero_1:
+        print(f'O primeiro é o maior número: {numero_1}')
+    else:
+        print(f'O segundo e o primeiro são iguais: {numero_2}, {numero_1}')
+elif numero_3 > numero_1:
+    if numero_3 > numero_2:
+        print(f'O terceiro é o maior número: {numero_3}')
+    elif numero_3 < numero_2:
+        print(f'O segundo é o maior número: {numero_2}')
+    else:
+        print(f'O terceiro e o segundo são iguais: {numero_3}, {numero_2}')
 else:
-    print('Existem números iguais')
+    print(f'Todos os números são iguasi: {numero_1}, {numero_2}, {numero_3}')
