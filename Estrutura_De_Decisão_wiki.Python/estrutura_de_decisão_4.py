97f08ed212181a437d804229b00b7f111fc18467
Windows10 <renankemiya@gmail.com>
2020-07-22 14:45:32 -0300

    terminado estrutura_de_decisão_4.py

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_4.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_4.py
@@ -2,9 +2,9 @@ letra = input('Insira uma letra: ')
 vogais = list('a' 'e' 'i' 'o' 'u')
 consoantes = list('b' 'c' 'd' 'f' 'g' 'h' 'j' 'k' 'l' 'm' 'n' 'p' 'q' 'r' 's' 't' 'v' 'w' 'x' 'y' 'z')
 for vogal in vogais:
-    if vogal == letra:
+    if letra == vogal:
         print('Essa letra é uma vogal')
 
 for consoante in consoantes:
-    if consoante == letra:
+    if letra == consoante:
         print('Essa letra é uma consoante')
