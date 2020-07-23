ed7d10e9f2386078ce2c6b30e3bd20b40c84c405
Windows10 <renankemiya@gmail.com>
2020-07-23 16:54:41 -0300

    terminado estrutura_de_decisão_9.py

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_9.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_9.py
@@ -3,22 +3,22 @@ numero_2 = float(input('Insira o segundo número: '))
 numero_3 = float(input('Insira o terceiro número: '))
 
 if numero_1 > numero_2 > numero_3:
-    print('Ordem: 1, 2, 3: ', numero_1, numero_2, numero_3)
+    print('Ordem: 1º, 2º, 3º: ', numero_1, numero_2, numero_3)
 elif numero_1 > numero_3 > numero_2:
-    print('Ordem: 1, 3, 2: ', numero_1, numero_3, numero_2)
+    print('Ordem: 1º, 3º, 2º: ', numero_1, numero_3, numero_2)
 elif numero_2 > numero_1 > numero_3:
-    print('Ordem: 2, 1, 3: ', numero_2, numero_1, numero_3)
+    print('Ordem: 2º, 1º, 3º: ', numero_2, numero_1, numero_3)
 elif numero_2 > numero_3 > numero_1:
-    print('Ordem: 2, 3, 1: ', numero_2, numero_3, numero_1)
+    print('Ordem: 2º, 3º, 1º: ', numero_2, numero_3, numero_1)
 elif numero_3 > numero_1 > numero_2:
-    print('Ordem: 3, 1, 2: ', numero_3, numero_1, numero_2)
+    print('Ordem: 3º, 1º, 2º: ', numero_3, numero_1, numero_2)
 elif numero_3 > numero_2 > numero_1:
-    print('Ordem: 3, 2, 1: ', numero_3, numero_2, numero_1)
+    print('Ordem: 3º, 2º, 1º: ', numero_3, numero_2, numero_1)
 elif numero_1 == numero_2 > numero_3:
-    print('Ordem: 1 e 2 iguais, 3: ', numero_1, numero_2, numero_3)
+    print('Ordem: 1º e 2º iguais, 3º: ', numero_1, numero_2, numero_3)
 elif numero_1 == numero_3 > numero_2:
-    print('Ordem: 1 e 3 iguais, 2: ', numero_1, numero_3, numero_2)
+    print('Ordem: 1º e 3º iguais, 2º: ', numero_1, numero_3, numero_2)
 elif numero_2 == numero_3 > numero_1:
-    print('Ordem: 2 e 3 iguais, 1: ', numero_2, numero_3, numero_1)
+    print('Ordem: 2º e 3º iguais, 1º: ', numero_2, numero_3, numero_1)
 else:
-    print('Ordem: 1, 2 e 3 são iguais: ', numero_1, numero_2, numero_3)
+    print('Ordem: 1º, 2º e 3º são iguais: ', numero_1, numero_2, numero_3)
