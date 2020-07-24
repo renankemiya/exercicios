a0c4a1a4ea76c640bd86feabdb6b0e098b3c90ee
Windows10 <renankemiya@gmail.com>
2020-07-24 15:23:55 -0300

    terminado estrutura_de_decisão_13.py

---
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_13.py
@@ -0,0 +1,69 @@
+dia = int(input('Insira um dia do mês: '))
+dias = {1: 'Domingo', 2: 'Segunda', 3: 'Terça', 4: 'Quarta', 5: 'Quinta', 6: 'Sexta', 7: 'Sábado',
+        8: 'Domingo', 9: 'Segunda', 10: 'Terça', 11: 'Quarta', 12: 'Quinta', 13: 'Sexta', 14: 'Sábado',
+        15: 'Domingo', 16: 'Segunda', 17: 'Terça', 18: 'Quarta', 19: 'Quinta', 20: 'Sexta', 21: 'Sábado',
+        22: 'Domingo', 23: 'Segunda', 24: 'Terça', 25: 'Quarta', 26: 'Quinta', 27: 'Sexta', 28: 'Sábado',
+        29: 'Domingo', 30: 'Segunda'}
+
+if dia == 1:
+    print(dias[1])
+elif dia == 2:
+    print(dias[2])
+elif dia == 3:
+    print(dias[3])
+elif dia == 4:
+    print(dias[4])
+elif dia == 5:
+    print(dias[5])
+elif dia == 6:
+    print(dias[6])
+elif dia == 7:
+    print(dias[7])
+elif dia == 8:
+    print(dias[8])
+elif dia == 9:
+    print(dias[9])
+elif dia == 10:
+    print(dias[10])
+elif dia == 11:
+    print(dias[11])
+elif dia == 12:
+    print(dias[12])
+elif dia == 13:
+    print(dias[13])
+elif dia == 14:
+    print(dias[14])
+elif dia == 15:
+    print(dias[15])
+elif dia == 16:
+    print(dias[16])
+elif dia == 17:
+    print(dias[17])
+elif dia == 18:
+    print(dias[18])
+elif dia == 19:
+    print(dias[19])
+elif dia == 20:
+    print(dias[20])
+elif dia == 21:
+    print(dias[21])
+elif dia == 22:
+    print(dias[22])
+elif dia == 23:
+    print(dias[23])
+elif dia == 24:
+    print(dias[24])
+elif dia == 25:
+    print(dias[25])
+elif dia == 26:
+    print(dias[26])
+elif dia == 27:
+    print(dias[27])
+elif dia == 28:
+    print(dias[28])
+elif dia == 29:
+    print(dias[29])
+elif dia == 30:
+    print(dias[30])
+else:
+    print('valor inválido')
