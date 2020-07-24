80879cd56b30ae5a54f991305124e9166c65a102
Windows10 <renankemiya@gmail.com>
2020-07-23 21:08:17 -0300

    terminado estrutura_de_decisão_11.py

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_11.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_11.py
@@ -5,34 +5,34 @@ if salario <= 280.00:
     percentual_aplicado = '20%'
     aumentar_salario = salario * 0.2
     salario_novo = salario + aumentar_salario
-    print(f'Salário antes do reajuste: {antes_do_reajuste}, '
+    print(f'Salário antes do reajuste: {antes_do_reajuste} reais, '
           f'Percentual aplicado: {percentual_aplicado}, '
-          f'Valor aumentado: {aumentar_salario}, '
-          f'Novo salário: {salario_novo}')
+          f'Valor aumentado: {aumentar_salario} reais, '
+          f'Novo salário: {salario_novo} reais')
 elif 280.00 < salario <= 700.00:
     antes_do_reajuste = salario
     percentual_aplicado = '15%'
     aumentar_salario = salario * 0.15
     salario_novo = salario + aumentar_salario
-    print(f'Salário antes do reajuste: {antes_do_reajuste}, '
+    print(f'Salário antes do reajuste: {antes_do_reajuste} reais, '
           f'Percentual aplicado: {percentual_aplicado}, '
-          f'Valor aumentado: {aumentar_salario}, '
-          f'Novo salário: {salario_novo}')
+          f'Valor aumentado: {aumentar_salario} reais, '
+          f'Novo salário: {salario_novo} reais')
 elif 700.00 < salario < 1500.00:
     antes_do_reajuste = salario
     percentual_aplicado = '10%'
     aumentar_salario = salario * 0.1
     salario_novo = salario + aumentar_salario
-    print(f'Salário antes do reajuste: {antes_do_reajuste}, '
+    print(f'Salário antes do reajuste: {antes_do_reajuste} reais, '
           f'Percentual aplicado: {percentual_aplicado}, '
-          f'Valor aumentado: {aumentar_salario}, '
-          f'Novo salário: {salario_novo}')
+          f'Valor aumentado: {aumentar_salario} reais, '
+          f'Novo salário: {salario_novo} reais')
 elif salario >= 1500.00:
     antes_do_reajuste = salario
     percentual_aplicado = '5%'
     aumentar_salario = salario * 0.05
     salario_novo = salario + aumentar_salario
-    print(f'Salário antes do reajuste: {antes_do_reajuste}, '
+    print(f'Salário antes do reajuste: {antes_do_reajuste} reais, '
           f'Percentual aplicado: {percentual_aplicado}, '
-          f'Valor aumentado: {aumentar_salario}, '
-          f'Novo salário: {salario_novo}')
+          f'Valor aumentado: {aumentar_salario} reais, '
+          f'Novo salário: {salario_novo} reais')
