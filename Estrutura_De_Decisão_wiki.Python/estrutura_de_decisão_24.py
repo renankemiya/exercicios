c1aad3e13e5bcaf09898088389d269db898493b4
Windows10 <renankemiya@gmail.com>
2020-07-28 14:58:45 -0300

    terminado estrutura_de_decisão_24.py

---
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_24.py
@@ -0,0 +1,85 @@
+numero_1 = float(input('Insira um número: '))
+numero_2 = float(input('Insira outro número: '))
+operacao = input('Insira a operação que deseja realizar: somar, subtrair, dividir ou multiplicar: ')
+resultado = 0
+
+if operacao == 'somar':
+    resultado = numero_1 + numero_2
+elif operacao == 'subtrair':
+    resultado = numero_1 - numero_2
+elif operacao == 'dividir':
+    resultado = numero_1 / numero_2
+elif operacao == 'multiplicar':
+    resultado = numero_1 * numero_2
+else:
+    print('Essa opereção é inválida, tente novamente')
+
+resto = resultado % 2
+checar_decimal = round(resultado)
+
+if resto == 0:
+    if resultado == checar_decimal:
+        if resultado > 0:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número par')
+            print(f'b. É um número positivo')
+            print(f'c. É um número inteiro')
+        elif resultado < 0:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número par')
+            print(f'b. É um número negativo')
+            print(f'c. É um número inteiro')
+        else:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número par')
+            print(f'b. É igual a 0')
+            print(f'c. É um número inteiro')
+    else:
+        if resultado > 0:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número par')
+            print(f'b. É um número positivo')
+            print(f'c. É um número decimal')
+        elif resultado < 0:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número par')
+            print(f'b. É um número negativo')
+            print(f'c. É um número decimal')
+        else:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número par')
+            print(f'b. É igual a 0')
+            print(f'c. É um número decimal')
+else:
+    if resultado == checar_decimal:
+        if resultado > 0:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número impar')
+            print(f'b. É um número positivo')
+            print(f'c. É um número inteiro')
+        elif resultado < 0:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número impar')
+            print(f'b. É um número negativo')
+            print(f'c. É um número inteiro')
+        else:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número impar')
+            print(f'b. É igual a 0')
+            print(f'c. É um número inteiro')
+    else:
+        if resultado > 0:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número impar')
+            print(f'b. É um número positivo')
+            print(f'c. É um número decimal')
+        elif resultado < 0:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número impar')
+            print(f'b. É um número negativo')
+            print(f'c. É um número decimal')
+        else:
+            print(f'O resultado da operação {operacao} foi {resultado}')
+            print(f'a. É um número impar')
+            print(f'b. É igual a 0')
+            print(f'c. É um número decimal')
