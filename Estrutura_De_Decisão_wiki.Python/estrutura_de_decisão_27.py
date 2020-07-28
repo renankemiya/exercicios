1d1be0eef1be63039fb153a35949750f822e4ebe
Windows10 <renankemiya@gmail.com>
2020-07-28 18:50:49 -0300

    terminado estrutura_de_decisão_27.py

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_27.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_27.py
@@ -8,40 +8,40 @@ if comprar == 'maças':
     peso = float(input('Quantidade de maças que deseja comprar em Kg: '))
     if peso <= 5:
         a_pagar = peso * ma_preco_ate_5
-        print(f'O valor de {peso} Kg de maças é de {a_pagar} reais')
+        print(f'O valor de {peso} Kg de maças é de {round(a_pagar,2)} reais')
     else:
         a_pagar = peso * ma_preco_maior_5
         if peso > 8 or a_pagar > 25:
             desconto_total = a_pagar * 0.1
             total_a_pagar = a_pagar - desconto_total
-            print(f'O valor de {peso} Kg de maças é de {total_a_pagar} reais')
+            print(f'O valor de {peso} Kg de maças é de {round(total_a_pagar,2)} reais')
         else:
-            print(f'O valor de {peso} Kg de maças é de {a_pagar} reais')
+            print(f'O valor de {peso} Kg de maças é de {round(a_pagar,2)} reais')
 elif comprar == 'morangos':
     peso = float(input('Quantidade de morangos que deseja comprar em Kg: '))
     if peso <= 5:
         a_pagar = peso * mo_preco_ate_5
-        print(f'O valor de {peso} Kg de morangos é de {a_pagar} reais')
+        print(f'O valor de {peso} Kg de morangos é de {round(a_pagar,2)} reais')
     else:
         a_pagar = peso * mo_preco_maior_5
         if peso > 8 or a_pagar > 25:
             desconto_total = a_pagar * 0.1
             total_a_pagar = a_pagar - desconto_total
-            print(f'O valor de {peso} Kg de morangos é de {total_a_pagar} reais')
+            print(f'O valor de {peso} Kg de morangos é de {round(total_a_pagar,2)} reais')
         else:
-            print(f'O valor de {peso} Kg de morangos é de {a_pagar} reais')
+            print(f'O valor de {peso} Kg de morangos é de {round(a_pagar,2)} reais')
 elif comprar == 'ambos':
     peso_macas = float(input('Quantidade de maças que deseja comprar em Kg: '))
     peso_morangos = float(input('Quantidade de morangos que deseja comprar em Kg: '))
     peso_total = peso_macas + peso_morangos
     if peso_total <= 5:
         preco_total = (peso_macas * ma_preco_ate_5) + (peso_morangos * mo_preco_ate_5)
-        print(f'{peso_total} Kg em maças e morangos irão custar {preco_total} reais')
+        print(f'{peso_total} Kg em maças e morangos irão custar {round(preco_total,2)} reais')
     else:
         preco_total = (peso_macas * ma_preco_maior_5) + (peso_morangos * mo_preco_maior_5)
         if peso_total > 8 or preco_total > 25:
             desconto_geral = preco_total * 0.1
             preco_a_pagar = preco_total - desconto_geral
-            print(f'{peso_total} Kg em maças e morangos irão custar {preco_a_pagar} reais')
+            print(f'{peso_total} Kg em maças e morangos irão custar {round(preco_a_pagar,2)} reais')
         else:
-            print(f'{peso_total} Kg em maças e morangos irão custar {preco_total} reais')
+            print(f'{peso_total} Kg em maças e morangos irão custar {round(preco_total,2)} reais')
