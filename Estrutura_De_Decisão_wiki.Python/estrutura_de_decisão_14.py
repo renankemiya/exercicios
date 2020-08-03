dad96cfe4d4c9f308d75d4159d103e132e8e7f91
Windows10 <renankemiya@gmail.com>
2020-08-03 16:20:46 -0300

    corrigido

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_14.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_14.py
@@ -1,8 +1,17 @@
+#  Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre,
+#  e calcule a sua média. A atribuição de conceitos obedece à tabela abaixo:
+#   Média de Aproveitamento  Conceito
+#   Entre 9.0 e 10.0        A
+#   Entre 7.5 e 9.0         B
+#   Entre 6.0 e 7.5         C
+#   Entre 4.0 e 6.0         D
+#   Entre 4.0 e zero        E
+
 nota_1 = float(input('Insira a primeira nota: '))
 nota_2 = float(input('Insira a segunda nota: '))
 media = (nota_1 + nota_2) / 2
 
-if 0 >= media > 0:
+if 4 >= media > 0:
     conceito = 'E'
     print(f'Primeira nota  : {nota_1}')
     print(f'Segunda nota   : {nota_2}')
@@ -37,3 +46,5 @@ elif 10 >= media > 9:
     print(f'Média          : {media}')
     print(f'Conceito       : {conceito}')
     print('APROVADO')
+
+#
