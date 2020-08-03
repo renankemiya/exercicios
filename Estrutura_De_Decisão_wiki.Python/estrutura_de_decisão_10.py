6475bdf07e43e5fc2e6a81172486f2f2e1f986ed
Windows10 <renankemiya@gmail.com>
2020-08-03 15:39:58 -0300

    corrigido

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_10.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_10.py
@@ -1,3 +1,6 @@
+#  Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
+#  Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
 turno = input('Insira o turno que você estuda, digite M para Matutino, V para Vespertino e N para Noturno: ')
 
 if turno == 'M':
@@ -8,3 +11,13 @@ elif turno == 'N':
     print('Boa Noite!')
 else:
     print('Valor Inválido!')
+
+# Correção da internet
+
+turno = input("digite seu turno M-matutino ou V-Vespertino ou N- Noturno ---> ")
+if turno == "M" or turno == "m":
+    print("bom dia")
+if turno == "V" or turno == "v":
+    print("boa tarde")
+if turno == "N" or turno == "n":
+    print("boa noite")
