48e7a3c6dbd250ed46ed0fc1ea5d96d43c583ada
Windows10 <renankemiya@gmail.com>
2020-08-03 14:31:33 -0300

    corrigindo

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_1.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_1.py
@@ -1,3 +1,5 @@
+#  Faça um Programa que peça dois números e imprima o maior deles.
+
 numero_1 = float(input('Insira um número: '))
 numero_2 = float(input('Insira outro número: '))
 
@@ -7,3 +9,15 @@ elif numero_2 > numero_1:
     print(f'O segundo número é maior: {numero_2}')
 else:
     print(f'Os números são iguais: {numero_1}')
+
+#  Correção da internet
+
+number1 = input("digite um numero: ")
+number2 = input("digite outro numero: ")
+
+if number1 > number2:
+    print("o maior numero eh ---> ", number1)
+if number2 > number1:
+    print("o maior numero eh ---> ", number2)
+else:
+    print(number1, " = ", number2)
