969e2550cea535002d1d8c8011436ae7046073dc
Windows10 <renankemiya@gmail.com>
2020-08-03 14:44:50 -0300

    corrigido

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_4.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_4.py
@@ -1,3 +1,5 @@
+#  Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+
 letra = input('Insira uma letra: ')
 vogais = list('a' 'e' 'i' 'o' 'u')
 consoantes = list('b' 'c' 'd' 'f' 'g' 'h' 'j' 'k' 'l' 'm' 'n' 'p' 'q' 'r' 's' 't' 'v' 'w' 'x' 'y' 'z')
@@ -8,3 +10,11 @@ for vogal in vogais:
 for consoante in consoantes:
     if letra == consoante:
         print('Essa letra é uma consoante')
+
+#  Correção da internet
+
+letra = input("digite uma letra (ex: \"a\") : ")
+if letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u":
+    print(letra, "eh uma vogal")
+else:
+    print(letra, "eh uma consoante")
