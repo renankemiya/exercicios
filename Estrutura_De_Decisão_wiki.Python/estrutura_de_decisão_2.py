ce2ff27e45706308f0b11ff64ac2204ff6ce8dac
Windows10 <renankemiya@gmail.com>
2020-08-03 14:36:28 -0300

    corrigindo

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_2.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_2.py
@@ -1,3 +1,5 @@
+#  Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
 valor = float(input('Insira um número: '))
 
 if valor > 0:
@@ -6,3 +8,13 @@ elif valor < 0:
     print('O número tem valor negativo')
 else:
     print('O valor é 0')
+
+# Correção da internet
+
+number1 = float(input("digite um numero: "))
+if number1 > 0:
+    print(" é positivo ", number1)
+if number1 < 0:
+    print(" é negativo ", number1)
+else:
+    print(" é ZERO ", number1)
