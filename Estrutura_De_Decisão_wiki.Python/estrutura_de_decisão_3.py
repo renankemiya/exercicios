7af5cea228cb105bd586a303467f0232e3712f0a
Windows10 <renankemiya@gmail.com>
2020-08-03 14:41:59 -0300

    corrigindo

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_3.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_3.py
@@ -1,3 +1,6 @@
+#  Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+#  Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+
 sexo = input('Insira F ou M para informar seu genero: ')
 
 if sexo == 'F':
@@ -6,3 +9,14 @@ elif sexo == 'M':
     print('Masculino')
 else:
     print('Sexo inválido')
+
+#  Correção da internet
+
+sexo = input("digite seu sexo \"F\" ou \"M\": ")
+if sexo == "F" or sexo == "M":
+    if sexo == "F":
+        print("F - Feminino")
+    else:
+        print("M - Masculino")
+else:
+    print("sexo invalido")
