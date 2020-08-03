a7dd5ecfeb9f716d01cc37503901aa79b7c1ac4d
Windows10 <renankemiya@gmail.com>
2020-08-03 15:31:02 -0300

    corrigido

--- Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_6.py
+++ Estrutura_De_Decisão_wiki.Python/estrutura_de_decisão_6.py
@@ -33,15 +33,9 @@ else:
 number1 = input("digite um numero ---> ")
 number2 = input("digite um numero ---> ")
 number3 = input("digite um numero ---> ")
-if (number1 > number2) and (number1 > number3) and (number2 > number3):
-    print(number1, " ", number2, " ", number3)
-if (number1 > number2) and (number1 > number3) and (number3 > number2):
-    print(number1, " ", number3, " ", number2)
-if (number2 > number1) and (number2 > number3) and (number1 > number3):
-    print(number2, " ", number1, " ", number3)
-if (number2 > number1) and (number2 > number3) and (number3 > number1):
-    print(number2, " ", number3, " ", number1)
-if (number3 > number1) and (number3 > number2) and (number1 > number2):
-    print(number3, " ", number1, " ", number2)
-if (number3 > number1) and (number3 > number2) and (number2 > number1):
-    print(number3, " ", number2, " ", number1)
+if (number1 > number2) and (number1 > number3):
+    print(number1, "eh o maior numero")
+if (number2 > number1) and (number2 > number3):
+    print(number2, "eh o maior numero")
+if (number3 > number1) and (number3 > number2):
+    print(number3, "eh o maior numero")
